02660e0735da1b7c51b99dd5a71c7427b5214cb5
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-04-25 21:16:59 -0500

    Generando funcion para eliminar paciente si le damos doble click

--- 03_Eliminacion, delegacion y animacion/js/eliminarPaciente.js
+++ 03_Eliminacion, delegacion y animacion/js/eliminarPaciente.js
@@ -1,2 +1,7 @@
 var pacientes = document.querySelectorAll(".paciente")
-console.log(pacientes)
+
+pacientes.forEach(function(paciente){
+    paciente.addEventListener("dblclick",function(){
+        this.remove();
+    });
+});
